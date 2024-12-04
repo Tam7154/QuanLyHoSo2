def29dbad28dc1553db029afa147ed56282d9a22
Tam7154 <117031511+Tam7154@users.noreply.github.com>
2024-12-04 19:39:42 +0700

    fix ui, show info of person

--- Assets/Script/InfoPerson.cs
+++ Assets/Script/InfoPerson.cs
@@ -7,6 +7,24 @@ public class InfoPerson
     public string name;//ten id cua quan nhan
     public Dictionary<string, object> infoPerson;//ten node + value node
     /*
+     * hinh xam
+     * benh ly
+     * toa an
+     * xu ly hanh chinh
+     * tham gia dang
+     * tham gia bieu tinh
+     * khieu nai
+     * tham gia ma tuy
+     * da tung di nuoc ngoai
+     * cho di nuoc ngoai
+     * mo coi
+     * vo
+     * co ai la can bo
+     * quan diem ve quan he dong gioi
+     * quan diem ve the che chinh tri
+     * ben noi
+     * ben ngoai
+     * 
      * 
      *thong tin anh chi em
      *
